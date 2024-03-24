42d76755af76d1124ce72f6a5cd9e96a911b32c5
fatiperea <fatimaperea3pppp0@gmail.com>
2024-03-24 18:51:49 -0300

    archivo estatico en ruta princ:index.html

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 import express from 'express'
+import 'dotenv/config' 
+import cors from 'cors'
+import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+
 
 const app=express()
 
 app.set("puerto",process.env.PORT || 4000)
 
 app.listen(app.get('puerto'), ()=>{console.log("puerto "+app.get('puerto'))})
+
+app.use(cors())
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: true}))
+
+const __filename= fileURLToPath(import.meta.url)
+
+const __dirname=path.dirname(__filename)
+
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res)=>{
+
+    res.send("respuesta backend")
+})
