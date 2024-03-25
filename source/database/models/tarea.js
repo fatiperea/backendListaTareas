8a00b14fadbc5399d1e7d6b50d754af9d11cf301
fatiperea <fatimaperea3pppp0@gmail.com>
2024-03-24 23:41:57 -0300

    creo modelo de tarea

--- source/database/models/tarea.js
+++ source/database/models/tarea.js
@@ -2,5 +2,9 @@ import mongoose, { Schema } from "mongoose";
 
 const tarea = new Schema({
   id: {String,unique:true},
-  tarea: { String, required: true, minLength: 3, maxLength: 20, unique: true }
+  tareaSchema: { String, required: true, minLength: 3, maxLength: 20, unique: true }
 });
+
+const Tarea= mongoose.model('tarea',tareaSchema)
+
+export default Tarea
