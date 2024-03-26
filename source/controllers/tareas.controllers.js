8fd050f82ee57edca97cbc7714a027a4958397fb
fatiperea <fatimaperea3pppp0@gmail.com>
2024-03-26 16:18:45 -0300

    completo la fc crear tarea

--- source/controllers/tareas.controllers.js
+++ source/controllers/tareas.controllers.js
@@ -1,3 +1,18 @@
-export const listarTareas=(req,res)=>{
-    res.send('enviando lista de tareas')
-}
+import Tarea from "../database/models/tarea.js";
+
+export const listarTareas = (req, res) => {
+  res.send("enviando lista de tareas");
+};
+
+export const crearTarea = async (req, res) => {
+  try {
+    const tareaNueva = new Tarea(req.body);
+
+    await tareaNueva.save();
+
+    res.status(201).json({ mensaje: "Tarea creada" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ mensaje: "La tarea no pudo ser creada" });
+  }
+};
--- source/database/models/tarea.js
+++ source/database/models/tarea.js
@@ -1,10 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 
-const tarea = new Schema({
-  id: {String,unique:true},
-  tareaSchema: { String, required: true, minLength: 3, maxLength: 20, unique: true }
+const tareaSchema = new Schema({
+  id: {
+    type: String,
+    unique: true
+  },
+  tarea: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+    unique: true
+  },
 });
 
-const Tarea= mongoose.model('tarea',tareaSchema)
+const Tarea = mongoose.model("tarea", tareaSchema);
 
-export default Tarea
+export default Tarea;
